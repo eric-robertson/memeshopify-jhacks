9dc00e05933cfd22bb9300e6f8584b86061aa5ba
Asher Gilani <gilaniasher@gmail.com>
2020-02-15 20:48:33 -0500

    Skeleton reddit bot code

---
+++ src/reddit_bot/mug_me_bot.py
@@ -0,0 +1,24 @@
+# TODO Add functionality for multiple subreddits at once
+# TODO Get karma for the bot
+
+import praw
+
+# Create Reddit instance using config from praw.ini
+reddit = praw.Reddit('MugMe-bot')
+subreddit = reddit.subreddit('me_irl')
+mugme_user = reddit.user.me()
+
+keyphrase = '!MugMeBot '
+
+# Check for keyphrase in comments
+for comment in subreddit.stream.comments():
+    if comment.body.startswith(keyphrase) and (comment not in mugme_user.saved(limit=None)):
+        print(comment.body)
+
+        # Save comment so bot doesn't reply again later
+        comment.save()
+
+        # Reply
+        comment.reply('Hello there from MugMeBot!')
+
+        print('Replied')
